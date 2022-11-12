4c8f4f068bade49fe5b91f8b3a09a2bf3080a110
vesela <mitkovska.vesela@uklo.edu.mk>
2022-11-12 21:45:28 +0100

    prv commit

---
+++ vezba1/MainClass.java
@@ -0,0 +1,13 @@
+package vezba1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Avtomobil av=new Avtomobil("Nissan","Opel","Puegot",256789, 1999, 
+				"BT-000-AA");
+				av.pMetod();
+				System.out.println(av.vMetod());
+				
+	}
+
+}
---
+++ vezba3/MainClass.java
@@ -0,0 +1,14 @@
+package vezba3;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        PC pc1 = new PC();
+		
+		pc1.prvMetod(4,500);
+
+
+	}
+
+}
